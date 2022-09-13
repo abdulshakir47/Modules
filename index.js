e4e7521fe6a446f02f3ad43292f236b76271a524
abdulshakir47 <112155594+abdulshakir47@users.noreply.github.com>
2022-09-13 10:27:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var array = [1,2,3,4,5,6]
+var array1 = [1,2,3,4,5,6]
+var array2 = [1,2,3,4,5,6]
+
+
+console.log(array);
+
+
+module.exports = {
+    array,array1,array2
+}
---
+++ module.js
@@ -0,0 +1,8 @@
+const {array,array1} = require('./index')
+const os = require("os")
+
+console.log(os.platform(),os.homedir());
+console.log(array,array1);
+
+
+
